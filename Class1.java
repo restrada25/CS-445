df3683f9c62a6ddeff465124db3db10e996ea537
restrada25 <restrad2@hawk.iit.edu>
2015-01-22 09:07:45 -0600

    Testing it out

--- Class1.java
+++ Class1.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 
 public class Class1 {
 	public static void main(String [] args){
+		Scanner scan=new Scanner(System.in);
 		System.out.println("I need to remember how to java");
-		
+		System.out.println("Enter something");
+		String input=scan.next();
+		System.out.println("Your input was "+input);
+				
 	}
 }
